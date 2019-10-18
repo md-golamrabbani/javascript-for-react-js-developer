8373ab9e1056a13e29eed5ed8b82771f7c069eec
md-golamrabbani <md.golamrabbani4700@gmail.com>
2019-10-18 20:14:15 +0600

    var keyword

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// var keyword 
+function hello() {
+    for(var i=0; i<5; i++) {
+        console.log(i);
+    }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+hello();
