1013f23beebbc85e818b884c445cab31e275513f
md-golamrabbani <md.golamrabbani4700@gmail.com>
2019-10-18 23:31:03 +0600

    modularity

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
-// inheritance
-class Person {
-    constructor(name) {
-        this.name = name;
-    }
-
-    walk() {
-        console.log("walk");
-    }
-}
-
-class Teacher extends Person {
-    constructor(name, degree) {
-        super(name);
-        this.degree = degree;
-    }
-
-    teach() {
-        console.log(this.name, this.degree);
-    }
-}
+// modularity
+import { Teacher } from "./teacher";
 
 const teacher = new Teacher("Golam Rabbani", "MSc");
 teacher.teach();
