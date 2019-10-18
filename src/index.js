ac185139c7783113c8c63878dec015b50e305c9e
md-golamrabbani <md.golamrabbani4700@gmail.com>
2019-10-18 22:16:18 +0600

    map method

--- src/index.js
+++ src/index.js
@@ -1,20 +1,4 @@
-// object diff
-const person = {
-    talk() {
-        // this is a windows object
-        // setTimeout(function() {
-        //     console.log("this", this);
-        // }, 1000);
-        // this is a person object
-        // var self = this;
-        // setTimeout(function() {
-        //     console.log("self", self);
-        // }, 1000);
-        // now, we are use fat arrow function.
-        setTimeout(() => {
-            console.log("this", this);
-        }, 1000);
-    }
-}
-
-console.log(person.talk());
+// map method
+const colors = ["red", "green", "blue"];
+const items = colors.map(color => `<li>${color}</li>`);
+console.log(items);
