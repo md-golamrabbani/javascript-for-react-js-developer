0069b67863db13274df7af8a81b15dd008944888
md-golamrabbani <md.golamrabbani4700@gmail.com>
2019-10-18 23:42:39 +0600

    default export

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-// modularity
-import { Teacher } from "./teacher";
+// default export
+import Teacher from "./teacher";
+//import Teacher, { promote } from "./teacher";
+//import React, { component } from "react";
+
+// default -> import ... from '';
+// Named -> import {...} from '';
 
 const teacher = new Teacher("Golam Rabbani", "MSc");
 teacher.teach();
