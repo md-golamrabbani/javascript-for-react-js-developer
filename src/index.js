3d02651f0bc8e216518f16d62f8631a305411f2a
md-golamrabbani <md.golamrabbani4700@gmail.com>
2019-10-18 21:43:38 +0600

    use of fat arrow function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-// normal function
-// const square = function(number) {
-//     return number*number;
-// }
-// console.log(square(5));
+// uses of fat arrow function
+const jobs = [
+    { id: 1, isActive: true},
+    { id: 2, isActive: true},
+    { id: 3, isActive: false}
+];
 
-// fat arrow function
-const square = (number) => number*number;
-console.log(square(5));
-
+//const activeJob = jobs.filter(function(job) {return job.isActive});
+const activeJob = jobs.filter(job => job.isActive);
+console.log(activeJob);
